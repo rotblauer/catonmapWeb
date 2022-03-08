f001f39acf80706aa52e4202a31ceec61eda725b
ia <isaac.ardis@gmail.com>
2022-03-08 07:04:17 -0800

    tag only on valid env
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- pushup.sh
+++ pushup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-git tag -a v$(date +%s) -m "[${1}] pushup"
-
 if [[ "$1" == "beta" ]]; then
+  git tag -a v$(date +%s) -m "[${1}] pushup"
 	rsync -avz -L --progress -h --exclude=".git" . sfcom:/www/beta.catonmap.net
 elif [[ "$1" == "pro" ]]; then
+  git tag -a v$(date +%s) -m "[${1}] pushup"
 	rsync -avz -L --progress -h --exclude=".git" . sfcom:/www/catonmap.net/
 else
 	echo "unknown arg"
