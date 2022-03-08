16ce68077ebdaa67a90f70fc584ed84606f03251
ia <isaac.ardis@gmail.com>
2022-03-08 06:48:45 -0800

    pushup.sh
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- pushup.sh
+++ pushup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+git tag -a v$(date +%s) -m "[${1}] pushup"
+
 if [[ "$1" == "beta" ]]; then
 	rsync -avz -L --progress -h --exclude=".git" . sfcom:/www/beta.catonmap.net
 elif [[ "$1" == "pro" ]]; then
