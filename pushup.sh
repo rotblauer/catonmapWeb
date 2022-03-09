379fed6db366e2a0792d3b18e396126f55eba0fc
ia <isaac.ardis@gmail.com>
2022-03-09 06:13:00 -0800

    :floppy_disk:
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- pushup.sh
+++ pushup.sh
@@ -2,15 +2,20 @@
 
 set -x
 
+push_target_beta=sfcom:/www/beta.catonmap.net
+push_target_pro=sfcom:/www/catonmap.net/
+push_target="$push_target_beta"
+
 if [[ "$1" == "beta" ]]; then
-  git tag -a v$(date +%s) -m "[${1}] pushup"
-	rsync -avz -L --progress -h --exclude=".git" . sfcom:/www/beta.catonmap.net
-  git push origin
 elif [[ "$1" == "pro" ]]; then
-  git tag -a v$(date +%s) -m "[${1}] pushup"
-	rsync -avz -L --progress -h --exclude=".git" . sfcom:/www/catonmap.net/
-  git push origin
+  push_target="$push_target_pro"
 else
 	echo "unknown arg"
+  exit 1
 fi
+
+git tag -a v$(date +%s) -m "[${1}] pushup"
+rsync -avz -L --progress -h --exclude=".git" . "$push_target"
+git push origin
+git push origin --tags
 
